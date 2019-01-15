d238d389a1757482af0ecb297c5b8d230db9fc52
NOTpanda <alexander113@126.com>
2019-01-15 16:13:13 +0000

    1. Refactor some methods & parameters; 2. Delete unused methods; 3. Delete some setters to enhance security.

--- src/com/taohong/Ticket.java
+++ src/com/taohong/Ticket.java
@@ -4,25 +4,28 @@ package com.taohong;
  * @author taohong on 2019-01-15
  */
 public class Ticket {
-    private int ticketNum;
+    private int tickNum;
     private int spaceNum;
     private String carNum;
     private boolean isParking;
 
-    public int getSpaceNum() {
-        return spaceNum;
-    }
-
-    public Ticket(int ticketNum, String carNum, int sapceNum) {
-        this.ticketNum = ticketNum;
+    public Ticket(int tickNum, String carNum, int sapceNum) {
+        this.tickNum = tickNum;
         this.carNum = carNum;
         this.spaceNum = sapceNum;
         this.isParking = true;
     }
 
+    public int getTickNum() {
+        return tickNum;
+    }
 
-    public int getTicketNum() {
-        return ticketNum;
+    public int getSpaceNum() {
+        return spaceNum;
+    }
+
+    public void setSpaceNum(int spaceNum) {
+        this.spaceNum = spaceNum;
     }
 
     public String getCarNum() {
